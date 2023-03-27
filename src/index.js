3358f3110523a9ae3b49639e9156700eb646075d
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-03-27 17:08:17 -0600

    Working React App with SQL database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';
+import { Link, HashRouter, Routes, Route, useParams } from 'react-router-dom';
 const root = createRoot(document.querySelector('#root'));
+
+const Content = ({content}) => {
+    const { id } = useParams();
+    const item = content.find(item => item.id === id * 1);
+    
+    if(!item){
+        return null;
+    }
+
+    return (
+        <div>
+            <h2>{ item.name }</h2>
+            <p>
+                { item.description }
+            </p>
+        </div>
+    );
+}
 
 const App = () => {
     const [content, setContent] = useState([]);
@@ -16,20 +35,25 @@ const App = () => {
 
     return (
         <div>
-            <h1>Personal Site (under construction) ({content.length})</h1>
+            <h1><Link to='/'>Personal Site (under construction) ({content.length})</Link></h1>
             <ul>
                 {
                     content.map( item => {
                         return (
-                            <li>
-                                {item.title}
+                            <li key={item.id}>
+                                <Link to={`/content/${item.id}`}>
+                                    {item.title}
+                                </Link>
                             </li>
                         );
                     })
                 }
             </ul>
+            <Routes>
+                <Route path='/content/:id' element={ <Content content={ content }/>} />
+            </Routes>
         </div>
     );
 }
 
-root.render(<App />);
+root.render(<HashRouter><App /></HashRouter>);
