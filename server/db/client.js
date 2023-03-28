3eafad249e2ba98df5327e901a9309c5e0a36b9f
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-03-27 19:36:13 -0600

    correctly dropping/creating tables for users and projects

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,12 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost' || process.env.DATABASE_URL );
+
+//The client may need to change to process.env.DATABASE_URL before deployment
+const client = new pg.Client({
+    user: 'quickbeam',
+    password: 'bilbo',
+    database: 'eac_personalsite_db',
+    host: 'localhost',
+    port: 5432
+});
 
 module.exports = client;
--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,45 @@
 const client = require('./client');
 
+const dropTables = async () => {
+    const SQL = `
+        DROP TABLE IF EXISTS projects;
+        DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS content;
+    `;
+    await client.query(SQL);
+}
+
+const syncTables = async () => {
+    const SQL = `
+        CREATE TABLE users(
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(50) NOT NULL UNIQUE,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255),
+            full_name VARCHAR(100),
+            bio TEXT,
+            website VARCHAR(255),
+            created_at TIMESTAMP DEFAULT NOW(),
+            isAdmin BOOLEAN default FALSE,
+            image_url VARCHAR(255)
+        );
+        CREATE TABLE projects(
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES users(id),
+            title VARCHAR(255) NOT NULL,
+            description TEXT NOT NULL,
+            project_url VARCHAR(255) NOT NULL,
+            image_url VARCHAR(255)
+        );
+    `;
+    await client.query(SQL);
+}
+
 const syncAndSeed = () => {
     console.log("Seeding");
+    dropTables();
+    syncTables();
+    console.log("Seeding complete")
 }
 
 module.exports = {
