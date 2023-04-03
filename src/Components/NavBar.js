9b28e420aab845d8cd8a95dfaa205cfa6a9882fa
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-04-03 09:35:19 -0600

    commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -16,15 +16,19 @@ const NavBar = () => {
         return () => window.removeEventListener('scroll', onScroll);
     }, [])
 
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
     return (
-        <>
+        <div className='navBar'>
             <nav className={scrolled ? 'scrolled' : ''}>
                 <Link to='/'><img className='logo' src={"https://s.yimg.com/wm/mbr/images/default-profile_192_v0.0.1.png"}/></Link>
-                <Link to='/admin' className={activeLink === 'admin' ? 'activeNavBarLink' : 'navBarLink'}>Admin</Link>
-                <Link to='/about' className={activeLink === 'about' ? 'activeNavBarLink' : 'navBarLink'}>About Me</Link>
-                <Link to='/skills' className={activeLink === 'skills' ? 'activeNavBarLink' : 'navBarLink'}>Skills</Link>
-                <Link to='/projects' className={activeLink === 'projects' ? 'activeNavBarLink' : 'navBarLink'}>Projects</Link>
-                <Link to='/contact' className={activeLink === 'contact' ? 'activeNavBarLink' : 'navBarLink'}>Contact</Link>
+                <Link to='/admin' className={activeLink === 'admin' ? 'activeNavBarLink' : 'navBarLink'} onClick={()=> onUpdateActiveLink('admin')}>Admin</Link>
+                <Link to='/about' className={activeLink === 'about' ? 'activeNavBarLink' : 'navBarLink'} onClick={()=> onUpdateActiveLink('about')}>About Me</Link>
+                <Link to='/skills' className={activeLink === 'skills' ? 'activeNavBarLink' : 'navBarLink'} onClick={()=> onUpdateActiveLink('skills')}>Skills</Link>
+                <Link to='/projects' className={activeLink === 'projects' ? 'activeNavBarLink' : 'navBarLink'} onClick={()=> onUpdateActiveLink('projects')}>Projects</Link>
+                <Link to='/contact' className={activeLink === 'contact' ? 'activeNavBarLink' : 'navBarLink'} onClick={()=> onUpdateActiveLink('contact')}>Contact</Link>
             </nav>
             <span className='navBar-text'>
                 <>
@@ -34,7 +38,7 @@ const NavBar = () => {
                 </>
                 <button className='social' onClick={()=> console.log("connect")}><span>Let's Connect</span></button>
             </span>
-        </>
+        </div>
     )
 };
 
